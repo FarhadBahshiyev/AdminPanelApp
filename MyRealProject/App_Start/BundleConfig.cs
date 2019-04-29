ff48d0ba694d34174c5c41778880bd052f3be5e5
FarhadBahshiyev <farxad144@gmail.com>
2019-04-29 23:14:16 +0300

    Initialcommit

--- MyRealProject/App_Start/BundleConfig.cs
+++ MyRealProject/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace MyRealProject
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+            //casdfadfav
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
